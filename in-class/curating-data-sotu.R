003c9ccbd597bc7c8eebe7d88e22a21bda386877
fjbeltrantapia <francisco.beltran.tapia@ntnu.no>
2026-03-03 09:34:47 +0100

    4 assign

---
+++ in-class/curating-data-sotu.R
@@ -0,0 +1,31 @@
+# Curating the data set extracted from the summaries of the 1860 census 
+  # for class assignments
+# Using the version stored in my website
+# Keeping only a few variables
+
+rm(list=ls())
+library(tidyverse)
+
+setwd("~/Library/CloudStorage/OneDrive-NTNU/course-websites/computational-history")
+
+sotu <- read_csv("data-assign/sotu/sotu.csv") |>
+  select(-`Word Count`) |>
+  rename(year = `Year`,
+         president = `President`,
+         text = `Text`)
+
+
+
+
+sotu <- sotu |>
+  distinct(year, president, text, .keep_all = TRUE)
+
+sotu |> count(year) |> print(n = Inf)
+
+# remove duplicates 
+pres <- sotu |>
+  count(president) |> select(president)
+
+write_csv(pres, "data-assign/sotu/presidents.csv")
+
+write_csv(pres, "data-assign/sotu/sotu-texts.csv")
